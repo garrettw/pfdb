e40a599093ca1f6b44d1c2cea350bd94f50eb0fd
Garrett W. <gw@garrettw.net>
2026-02-18 20:37:18 +0000

    fix deployment

--- .platform/hooks/postdeploy/03_laravel_setup.sh
+++ .platform/hooks/postdeploy/03_laravel_setup.sh
@@ -4,3 +4,9 @@
 cd /var/app/current
 
 php artisan optimize
+
+# Fix ownership and permissions in case they changed
+chown -R webapp:webapp storage bootstrap/cache
+chmod -R 775 storage bootstrap/cache
+chown webapp:webapp storage/logs/laravel.log
+chmod 664 storage/logs/laravel.log
